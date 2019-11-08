dc63135c647908d5c15002f45a8522eb126e3090
ambrozin <46778101+ambrozin@users.noreply.github.com>
2019-11-08 10:09:15 +0100

    Added funtions to simulate array response; beamforming based on plane waves

---
+++ tools/simulate_array_response.m
@@ -0,0 +1,10 @@
+function Res = simulate_array_response(X,Y,target,exct,f_vec,k_a, k_s)
+
+Res = zeros(size(X,2), exct.n_sampl);
+for i = 1:size(X,2)
+        d = sqrt((X(i)-target.x).^2+(Y(i)-target.y).^2); 
+        resp_a = dispResponse(f_vec,k_a,exct.fs,exct.n_sampl,exct.f_exc,exct.n_cycl,d);
+%         resp_s = dispResponse(f_vec,k_s,exct.fs,exct.n_sampl,exct.f_exc,exct.n_cycl,d);
+        Res(i,:) =  hilbert(resp_a );
+    disp(i/size(X,2)*100)
+end
